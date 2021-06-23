58184bf42a7728e5437b6108fa0ec91c1ae2ab12
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-23 12:56:33 -0700

    aUPDATE_PRODUCTS test

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,17 @@
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from './actions';
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products],
+            };
+        default:
+            return state;
+    }
+}
