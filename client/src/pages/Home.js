978782f8ce2a6d4a282c79ad3a772e4f75476a64
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-23 14:08:27 -0700

    added state to Detail.js and Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,13 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 
 const Home = () => {
-  const [currentCategory, setCategory] = useState("");
-
-  return (
-    <div className="container">
-      <CategoryMenu setCategory={setCategory} />
-      <ProductList currentCategory={currentCategory} />
+  return(
+    <div className='container'>
+      <CategoryMenu />
+      <ProductList />
     </div>
-  );
+    
+  )
 };
 
 export default Home;
