16b24f330c1f6d48977d732d6df7fe7b8e2c58f2
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-29 12:40:25 -0700

    items added to cart are persistent

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { ADD_MULTIPLE_TO_CART, TOGGLE_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({type: ADD_MULTIPLE_TO_CART, products: [...cart]});
+        };
 
-    console.log(state)
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -30,6 +40,7 @@ const Cart = () => {
             </div>
         )
     }
+
 
     return (
         <div className="cart">
